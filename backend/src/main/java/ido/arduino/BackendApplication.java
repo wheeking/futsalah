587371f5a998c0c0a38cf9632292fe470f0020ee
김동휘 <wheeking@gmail.com>
2020-09-06 14:26:02 +0900

    copy to github

--- backend/src/main/java/ido/arduino/BackendApplication.java
+++ backend/src/main/java/ido/arduino/BackendApplication.java
@@ -16,7 +16,7 @@ public class BackendApplication {
 	private static final String PROPERTIES = "classpath:/mysql.yml" + ",classpath:/aws.yml"
 			+ ",classpath:/application.properties";
 
-	public static void main(String[] args) throws JSchException, InterruptedException {
+	public static void main(String[] args) {
 		System.setProperty("spring.config.location", PROPERTIES);
 		SpringApplication.run(BackendApplication.class, args);
 		
--- backend/src/main/java/ido/arduino/controller/CourtController.java
+++ backend/src/main/java/ido/arduino/controller/CourtController.java
@@ -60,72 +60,83 @@ public class CourtController {
 		} else if (data.getAwayLate() == 2) {
 			courtService.reliability(data.getAway(), 10);
 		}
-		
-		// 라즈베리파이 파이썬 코드 실행
-		String rasp_name = "pi";
-		String host1 = "192.168.219.107";
-		String host2 = "192.168.219.104";
-		int port = 22;
-		String password = "root";
-
-		System.out.println("==> Connecting to " + host1);
-		Session session1 = null;
-		Session session2 = null;
-		Channel channel1 = null;
-		Channel channel2 = null;
-
-		try {
-			// JSch 객체 생성
-			JSch jsch1 = new JSch();
-			JSch jsch2 = new JSch();
-			session1 = jsch1.getSession(rasp_name, host1, port);
-			session2 = jsch2.getSession(rasp_name, host2, port);
-
-			// 패스워드 설정
-			session1.setPassword(password);
-			session2.setPassword(password);
-
-			// 세션 관련 정보 설정
-			java.util.Properties config = new java.util.Properties();
-			// 호스트 정보 검사 x
-			config.put("StrictHostKeyChecking", "no");
-			session1.setConfig(config);
-			session2.setConfig(config);
-
-			// 접속
-			session1.connect();
-			session2.connect();
-
-			// sftp 채널 오픈
-			channel1 = session1.openChannel("exec");
-			channel2 = session2.openChannel("exec");
-
-			// SSH용 채널 객체로 캐스팅
-			ChannelExec channelExec1 = (ChannelExec) channel1;
-			ChannelExec channelExec2 = (ChannelExec) channel2;
-			System.out.println("==> Connected to " + host1);
-
-			channelExec1.setCommand("python3 /home/pi/Desktop/goal.py");
-			channelExec2.setCommand("python3 /home/pi/Desktop/goal2.py");
-
-			channelExec1.connect();
-			channelExec2.connect();
-			System.out.println("==> Connected to " + host1);
-
-		} finally {
-			if (channel1 != null) {
-				channel1.disconnect();
-			}
-			if (channel2 != null) {
-				channel2.disconnect();
-			}
-			if (session1 != null) {
-				session1.disconnect();
-			}
-			if (session2 != null) {
-				session2.disconnect();
-			}
-		}
+//		
+//		// 라즈베리파이 파이썬 코드 실행
+//		String rasp_name = "pi";
+//		String host1 = "192.168.219.107";
+//		String host2 = "192.168.219.111";
+//		int port = 22;
+//		int port2 = 23;
+//		String password = "root";
+//
+//		Session session1 = null;
+//		Session session2 = null;
+//		Channel channel1 = null;
+//		Channel channel2 = null;
+//
+//		try {
+//			// JSch 객체 생성
+//			JSch jsch1 = new JSch();
+//			JSch jsch2 = new JSch();
+//			session1 = jsch1.getSession(rasp_name, host1, port);
+//			System.out.println("<<<<<<<<<<<<");
+//			session2 = jsch2.getSession(rasp_name, host2, port);
+//			System.out.println("<<<<<<<<<<<<");
+//
+//			// 패스워드 설정
+//			session1.setPassword(password);
+//			System.out.println("<<<<<<<<<<<<");
+//			session2.setPassword(password);
+//			System.out.println("<<<<<<<<<<<<");
+//
+//			// 세션 관련 정보 설정
+//			java.util.Properties config = new java.util.Properties();
+//			// 호스트 정보 검사 x
+//			config.put("StrictHostKeyChecking", "no");
+//			session1.setConfig(config);
+//			System.out.println("<<<<<<<<<<<<");
+//			session2.setConfig(config);
+//			System.out.println("<<<<<<<<<<<<");
+//
+//			// 접속
+//			//session1.connect();
+//			System.out.println("<<<<<<<<<<<<");
+//			session2.connect();
+//			System.out.println("<<<<<<<<<<<<");
+//
+//			// sftp 채널 오픈
+//			//channel1 = session1.openChannel("exec");
+//			System.out.println("<<<<<<<<<<<<");
+//			channel2 = session2.openChannel("exec");
+//			System.out.println("<<<<<<<<<<<<");
+//
+//			// SSH용 채널 객체로 캐스팅
+//			ChannelExec channelExec1 = (ChannelExec) channel1;
+//			ChannelExec channelExec2 = (ChannelExec) channel2;
+//
+//			//channelExec1.setCommand("python3 /home/pi/Desktop/goal.py");
+//			System.out.println("<<<<<<<<<<<<");
+//			channelExec2.setCommand("python3 /home/pi/Desktop/goal2.py");
+//
+//			//channelExec1.connect();
+//			System.out.println("<<<<<<<<<<<<");
+//			channelExec2.connect();
+//			System.out.println("<<<<<<<<<<<<");
+//
+//		} finally {
+//			if (channel1 != null) {
+//				channel1.disconnect();
+//			}
+//			if (channel2 != null) {
+//				channel2.disconnect();
+//			}
+//			if (session1 != null) {
+//				session1.disconnect();
+//			}
+//			if (session2 != null) {
+//				session2.disconnect();
+//			}
+//		}
 	}
 
 	@ApiOperation(value = "게임 종료", response = String.class)
@@ -170,7 +181,7 @@ public class CourtController {
 		String rasp_name = "pi";
 		// 라즈베리파이 ip
 		String host1 = "192.168.219.107";
-		String host2 = "192.168.219.104";
+		String host2 = "192.168.219.111";
 		int port = 22;
 		String password = "root";
 
